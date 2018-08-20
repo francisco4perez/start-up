57900ba5a316ac58f1d55e8484003a6859f3cbed
Francisco <francisco.perezca.13@sansano.usm.cl>
2018-08-19 21:18:12 -0300

    initial commit

---
+++ notepad/resources/views/admin/template/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Una plantilla normal</title>
+	<link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.css') }}">
+	<script src=" {{ asset('js/bootstrap/bootstrap.js') }} "></script>
+	<script src=" {{ asset('js/jquery/jquery.min.js') }} " ></script>
+</head>
+<body>
+	
+</body>
+</html>

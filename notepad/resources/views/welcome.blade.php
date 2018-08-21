1de1d5aa616222e035fa00ed3d84dc33b915a239
francisco4perez <francisco.perezca.13@sansano.usm.cl>
2018-08-21 20:04:01 -0300

    css: alineacion nav, shadow nav links. Se añadió logo onecrowd.

--- notepad/resources/views/welcome.blade.php
+++ notepad/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hola!!</title>
+    <title>Aqualert!</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/mystyles.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/flexboxgrid.css') }}">
     <script type="text/javascript" src=" {{ asset('js/jquery-3.3.1.js') }} "></script>
@@ -16,7 +16,7 @@
     </div>
 
     <div id="portion1" class="info">
-      <h3>Heading for Portion 1</h3>
+      <h3>Explicacion extremadamente hermosa</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis nobis ut dicta quisquam officia deleniti, amet. Obcaecati atque, cumque quae culpa ad hic commodi provident pariatur, iure nobis voluptatibus laborum amet temporibus quia, dolores veritatis, repellendus consectetur. Ut ullam officiis minus quod assumenda facere quaerat, esse, magni, aliquid culpa vero.</p>
       <div class="button">
         <a href="#">Example Button</a>
@@ -24,11 +24,11 @@
     </div>
 
     <div class="portion two">
-      <h2>Portion 2</h2>
+      <h2>OneCrowd</h2>
     </div>
 
     <div id="portion2" class="info">
-      <h3>Heading for Portion 2</h3>
+      <h3>Equipo OneCrowd</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora pariatur voluptate laboriosam impedit praesentium sed, nihil, dignissimos et minima recusandae quaerat enim consectetur. Molestiae assumenda distinctio, rem nostrum dolores repellendus.</p>
       <div class="button">
         <a href="#">Example Button</a>
@@ -36,11 +36,11 @@
     </div>
 
     <div class="portion">
-      <h2>Portion 3.</h2>
+      <h2>Nosotros</h2>
     </div>
 
     <div id="portion3" class="info">
-      <h3>Heading for Portion 3.</h3>
+      <h3>Nosotros</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio accusamus in vitae saepe harum soluta reiciendis eaque repellat distinctio quos voluptate nemo error ratione numquam, nisi quibusdam veritatis repudiandae ea possimus rerum pariatur dolores? Optio inventore iusto, voluptatibus numquam aperiam, harum maxime beatae minima aliquam quas sapiente totam cumque unde..</p>
       <div class="button">
         <a href="#">Example Button</a>
@@ -48,11 +48,11 @@
     </div>
 
     <div class="portion two">
-      <h2>Portion 4.</h2>
+      <h2>Contacto</h2>
     </div>
 
     <div id="portion4" class="info">
-      <h3>Heading for Portion 4.</h3>
+      <h3>Contacto</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni incidunt quos iste voluptates, tenetur ad repudiandae ea fuga eveniet quam, unde iure suscipit rem odit in, sint nulla itaque? Labore beatae, est voluptatibus saepe rerum illum repudiandae quasi perspiciatis, molestiae quidem fugiat voluptates voluptate neque totam earum enim mollitia iure quod tempora veritatis quam optio. Error odit laudantium eum voluptate.</p>
       <div class="button">
         <a href="#">Example Button</a>

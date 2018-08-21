d509e3abb9619080e720e3c08fca9dfd1232c238
francisco4perez <francisco.perezca.13@sansano.usm.cl>
2018-08-20 23:30:32 -0300

    css: logo, water_drop and colors

--- notepad/resources/views/header/header.blade.php
+++ notepad/resources/views/header/header.blade.php
@@ -1,8 +1,8 @@
 <header>
   <nav>
     <span class="movetext">Company Name</span>
-    <img class="" src="https://www.matchbook.com/assets/images/icons/white/Twitter_logo_white.png">
-    <a href="#portion1" class="active">Portion1</a>
+    <img class="logo" src=" {{ asset('image/aqualert_logo.svg') }} ">
+    <a href="#portion1">Portion1</a>
     <a href="#portion2">Portion2</a>
     <a href="#portion3">Portion3</a>
     <a href="#portion4">Portion4</a>

2da622344ded5a2746abf3e917f7bf85d15b1321
francisco4perez <francisco.perezca.13@sansano.usm.cl>
2018-09-04 00:10:36 -0300

    fixing map, css problems, added backgrounds to sections and arrow icons to buttons

--- notepad/resources/views/google-map.blade.php
+++ notepad/resources/views/google-map.blade.php
@@ -1,18 +1,25 @@
 <script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyCkOqMTxzwN0XRYpW6oRWfV-cQ0v0xWi1g'></script>
-<!-- Google map code from EZ Map - https://ezmap.co -->
+
 <script>
-  function init() {
-    var mapOptions = { "center": {  "lat": -33.48981917448171,  "lng": -70.61843617116392 }, "clickableIcons": false, "disableDoubleClickZoom": true, "draggable": false, "fullscreenControl": true, "keyboardShortcuts": false, "mapMaker": false, "mapTypeControl": false, "mapTypeControlOptions": {  "text": "Default (depends on viewport size etc.)",  "style": 0 }, "mapTypeId": "roadmap", "rotateControl": true, "scaleControl": false, "scrollwheel": false, "streetViewControl": false, "styles": false, "zoom": 16, "zoomControl": true};
-    var mapElement = document.getElementById('ez-map');
-    var map = new google.maps.Map(mapElement, mapOptions);
-    var marker0 = new google.maps.Marker({title: "Feria del Software 2018 / Vicuña Mackenna 3939, Santiago, San Joaquín", icon: "https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi.png", position: new google.maps.LatLng(-33.48981917448171, -70.61843617116392), map: map});
-var infowindow0 = new google.maps.InfoWindow({content: "<h3 class=\"infoTitle\">Feria del Software 2018 / Vicuña Mackenna 3939, Santiago, San Joaquín</h3><p></p>",map: map});
-	infowindow0.open(map, marker0) ;
-    google.maps.event.addDomListener(window, "resize", function() { var center = map.getCenter(); google.maps.event.trigger(map, "resize"); map.setCenter(center); });
-  }
-google.maps.event.addDomListener(window, 'load', init);
+  $( document ).ready(function() {
+
+    function init() {
+      var mapOptions = { "center": {  "lat": -33.48981917448171,  "lng": -70.61843617116392 }, "clickableIcons": false, "disableDoubleClickZoom": true, "draggable": false, "fullscreenControl": true, "keyboardShortcuts": false, "mapMaker": false, "mapTypeControl": false, "mapTypeControlOptions": {  "text": "Default (depends on viewport size etc.)",  "style": 0 }, "mapTypeId": "roadmap", "rotateControl": true, "scaleControl": false, "scrollwheel": false, "streetViewControl": false, "styles": false, "zoom": 16, "zoomControl": true};
+      var mapElement = document.getElementById('ez-map');
+      var map = new google.maps.Map(mapElement, mapOptions);
+      var marker0 = new google.maps.Marker({title: "Feria del Software 2018 / Vicuña Mackenna 3939, Santiago, San Joaquín", icon: "https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi.png", position: new google.maps.LatLng(-33.48981917448171, -70.61843617116392), map: map});
+    var infowindow0 = new google.maps.InfoWindow({content: "<h3 align='center' class=\"infoTitle\">Feria del Software 2018 <br> Vicuña Mackenna 3939, Santiago, San Joaquín</h3><p></p>",map: map});
+    marker0.addListener('click', function () { infowindow0.open(map, marker0) ;});infowindow0.close();
+    infowindow0.open(map, marker0);
+
+      google.maps.event.addDomListener(window, "resize", function() { var center = map.getCenter(); google.maps.event.trigger(map, "resize"); map.setCenter(center); });
+    }
+    google.maps.event.addDomListener(window, 'load', init);
+
+  });
+  
 </script>
 
 <div id='ez-map'></div>
-<!-- End of EZ Map code - https://ezmap.co -->
+
 
--- notepad/resources/views/welcome.blade.php
+++ notepad/resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/flexboxgrid.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/magnific-popup.css') }}">
     <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <script type="text/javascript" src=" {{ asset('js/jquery-3.3.1.js') }} "></script>
     <script type="text/javascript" src=" {{ asset('js/magnific-popup.min.js') }} "></script>
@@ -21,7 +22,7 @@
         <div id="title">
           <h2>Sistema de alertas para tu agua</h2>
 
-          <div id="div-btn" class="popup-youtube" href="http://www.youtube.com/watch?v=0O2aH4XLbto">
+          <div id="div-btn" class="popup-youtube" href="https://www.youtube.com/watch?v=Rzv5a8viDAw">
             <div class="fancy-btn">
               <a>Ver&nbsp;Video&nbsp;<span class="shift">›</span></a>
               <div class="mask"></div>
@@ -37,11 +38,8 @@
       <h3>Aqualert, el siste de alertas inteligentes</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis nobis ut dicta quisquam officia deleniti, amet. Obcaecati atque, cumque quae culpa ad hic commodi provident pariatur, iure nobis voluptatibus laborum amet temporibus quia, dolores veritatis, repellendus consectetur. Ut ullam officiis minus quod assumenda facere quaerat, esse, magni, aliquid culpa vero.</p>
       
-      <div id="div-btn">
-        <div class="fancy-btn grey">
-          <a href="#portion2">Seguir&nbsp;Bajando&nbsp;<span class="shift">›</span></a>
-          <div class="mask"></div>
-        </div>
+      <div class="button">
+         <a href="#portion2">Seguir bajando <img class="expand-icon" src="{{ asset('icon/expand-button-down.png ') }}"></a>
       </div>
 
     </div>
@@ -76,12 +74,14 @@
     <div id="portion2" class="info">
       <h3>Aqualert</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora pariatur voluptate laboriosam impedit praesentium sed, nihil, dignissimos et minima recusandae quaerat enim consectetur. Molestiae assumenda distinctio, rem nostrum dolores repellendus.</p>
+      
       <div class="button">
-        <a href="#portion3">Example Button</a>
+         <a href="#portion3">Seguir bajando <img class="expand-icon" src="{{ asset('icon/expand-button-down.png ') }}"></a>
       </div>
+
     </div>
 
-    <div class="my-section">
+    <div class="my-section two">
       <h2>Nosotros</h2>
     </div>
 
@@ -92,30 +92,36 @@
       <div class="my-container">
         <div class="row center">
 
-          <div class="col">
+          <div class="col-xs-12 col-sm-4 col-md-4">
               <div class="box">
-                <img class="team-icon" src="{{ asset('image/user.png ') }}">
-                <h4>Nombre</h4>
-                <h5>Web Developer</h5>
-                <p>Linkedin</p>
+                <div class="user">
+                  <img class="team-icon" src="{{ asset('image/user.png ') }}">
+                  <h4>Alfredo Silva</h4>
+                  <h5>Web Developer</h5>
+                  <p>Linkedin</p>
+                </div>
               </div>
           </div>
 
-          <div class="col">
+          <div class="col-xs-12 col-sm-4 col-md-4">
               <div class="box">
-                <img class="team-icon" src="{{ asset('image/user.png ') }}">
-                <h4>Nombre</h4>
-                <h5>Web Developer</h5>
-                <p>Linkedin</p>
+                <div class="user">
+                  <img class="team-icon" src="{{ asset('image/user.png ') }}">
+                  <h4>Francisco Pérez</h4>
+                  <h5>Web Developer</h5>
+                  <p>Linkedin</p>
+                </div>
               </div>
           </div>
 
-          <div class="col">
+          <div class="col-xs-12 col-sm-4 col-md-4">
               <div class="box">
-                <img class="team-icon" src="{{ asset('image/user.png ') }}">
-                <h4>Nombre</h4>
-                <h5>Web Developer</h5>
-                <p>Linkedin</p>
+                <div class="user">
+                  <img class="team-icon" src="{{ asset('image/user.png ') }}">
+                  <h4>Francisco Alvial</h4>
+                  <h5>Web Developer</h5>
+                  <p>Linkedin</p>
+                </div>
               </div>
           </div>
 
@@ -127,56 +133,66 @@
       <div class="my-container">
         <div class="row">
           
-          <div class="col">
+          <div class="col-xs-12 col-sm-6 col-md-6">
               <div class="box">
-                <img class="team-icon" src="{{ asset('image/user.png ') }}">
-                <h4>Nombre</h4>
-                <h5>Web Developer</h5>
-                <p>Linkedin</p>
+                <div class="user">
+                  <img class="team-icon" src="{{ asset('image/user.png ') }}">
+                  <h4>Juan Escalona</h4>
+                  <h5>Web Developer</h5>
+                  <p>Linkedin</p>
+                </div>
               </div>
           </div>
 
-          <div class="col">
+          <div class="col-xs-12 col-sm-6 col-md-6">
               <div class="box">
-                <img class="team-icon" src="{{ asset('image/user.png ') }}">
-                <h4>Nombre</h4>
-                <h5>Web Developer</h5>
-                <p>Linkedin</p>
+                <div class="user">
+                  <img class="team-icon" src="{{ asset('image/user.png ') }}">
+                  <h4>Sebastián Torrico</h4>
+                  <h5>Web Developer</h5>
+                  <p>Linkedin</p>
+                </div>
               </div>
           </div>
+
 
         </div>
       </div>
 
       <div class="button">
-        <a href="#portion4">Example Button</a>
+         <a href="#portion4">Seguir bajando <img class="expand-icon" src="{{ asset('icon/expand-button-down.png ') }}"></a>
       </div>
+
     </div>
 
-    <div class="my-section">
+    <div class="my-section three">
       <h2>Visítenos</h2>
     </div>
 
     <div id="portion4" class="info">
       <h3>Visítenos</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni incidunt quos iste voluptates, tenetur ad repudiandae ea fuga eveniet quam, unde iure suscipit rem odit in, sint nulla itaque? Labore beatae, est voluptatibus saepe rerum illum repudiandae quasi perspiciatis, molestiae quidem fugiat voluptates voluptate neque totam earum enim mollitia iure quod tempora veritatis quam optio. Error odit laudantium eum voluptate.</p>
+      
       <div class="button">
-        <a href="#portion5">Example Button</a>
+         <a href="#portion5">Seguir bajando <img class="expand-icon" src="{{ asset('icon/expand-button-down.png ') }}"></a>
       </div>
+
     </div>
 
     @include('google-map')
 
-    <div class="my-section">
+    <div class="my-section four">
       <h2>Contáctanos</h2>
     </div>
 
     <div id="portion5" class="info">
       <h3>Contáctanos</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni incidunt quos iste voluptates, tenetur ad repudiandae ea fuga eveniet quam, unde iure suscipit rem odit in, sint nulla itaque? Labore beatae, est voluptatibus saepe rerum illum repudiandae quasi perspiciatis, molestiae quidem fugiat voluptates voluptate neque totam earum enim mollitia iure quod tempora veritatis quam optio. Error odit laudantium eum voluptate.</p>
+
       <div class="button">
-        <a href="#top">Volver al inicio</a>
+         <a href="#top">Volver al inicio <img class="expand-icon" src="{{ asset('icon/expand-button-up.png ') }}"></a>
       </div>
+
     </div>
     
     @include('footer.footer')

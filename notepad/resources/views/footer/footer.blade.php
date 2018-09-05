33fba4cccaf91e70a466f408fc30b7955e866397
francisco4perez <francisco.perezca.13@sansano.usm.cl>
2018-09-05 12:39:48 -0300

    sponsors full responsive added in footer

--- notepad/resources/views/footer/footer.blade.php
+++ notepad/resources/views/footer/footer.blade.php
@@ -1,4 +1,55 @@
 <footer class="info">
-	  <div id="toTop"><a href="#top"><img src="{{ asset('image/onecrowd.svg ') }}"></a></div>
-	  <p>&copy; 2018 OneCrowd Team</p>
+	<div class="my-container">
+        <div class="row center">
+
+          <div class="col-xs-12 col-sm-4 col-md-4">
+              <div class="box">
+                <div>
+                  <img class="sponsor usm-icon" src="{{ asset('image/usm.png ') }}">
+                </div>
+              </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-4 col-md-4">
+              <div class="box">
+                <div>
+                  <img class="sponsor di-icon" src="{{ asset('image/di.png ') }}">
+                </div>
+              </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-4 col-md-4">
+              <div class="box">
+                <div>
+                  <img class="sponsor fesw-icon" src="{{ asset('image/feriasw.png ') }}">
+                </div>
+              </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-6">
+              <div class="box">
+                <div>
+                  <img class="sponsor piea-icon" src="{{ asset('image/piea.png ') }}">
+                </div>
+              </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-6">
+              <div class="box">
+              	<a id="#top">
+              		<img class="sponsor one-crowd" src="{{ asset('image/onecrowd.svg ') }}">
+	  				<p class="no-margin">&copy; 2018 OneCrowd Team</p>
+	  			</a>
+              </div>
+          </div>
+
+         </div>
+
+
+        </div>
+
+        
+      </div>
+
+	  
 </footer>
